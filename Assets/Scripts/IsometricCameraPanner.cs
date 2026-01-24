7fc81f5e82c67424a3613d1760682f310fe1bed6
ralfpaat <paatralf@gmail.com>
2026-01-24 14:25:16 +0200

    Made working camera panning

---
+++ Assets/Scripts/IsometricCameraPanner.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class IsometricCameraPanner : MonoBehaviour
+{
+    public float panSpeed = 6f;
+
+    public InputActionReference Move;
+    private Vector2 moveDirection;
+
+    public PolygonCollider2D movemntBounds;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //bool enabled = Move.action.enabled;
+        moveDirection = Move.action.ReadValue<Vector2>();
+
+        //Debug.Log($"Move Enabled: {enabled}, Value: {moveDirection}");
+        Vector3 newPosition = transform.position + new Vector3(moveDirection.x, moveDirection.y, 0) * (panSpeed * Time.deltaTime);
+        Vector2 newPosition2D = new Vector2(newPosition.x, newPosition.y);
+
+        if(movemntBounds.OverlapPoint(newPosition2D))
+        {
+            transform.position = newPosition;
+        }
+        else
+        {
+            Vector2 closest = movemntBounds.ClosestPoint(newPosition2D);
+            transform.position = new Vector3(closest.x, closest.y, transform.position.z);
+        }
+
+        //transform.position += new Vector3(moveDirection.x, moveDirection.y, 0) * (panSpeed * Time.deltaTime);
+        
+    }
+}
