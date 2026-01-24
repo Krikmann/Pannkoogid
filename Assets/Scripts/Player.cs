2dbdf1a7354af0aa7a71e7dc199c349e9807c036
Kristo Krikmann <kristo.krikmann@gmail.com>
2026-01-24 14:47:28 +0200

    wip zoom in select

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,54 +1,5 @@
-using System;
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
-    public InputActionReference move;
-    public InputActionReference select;
-    
-    private GameObject hoveredCharacter;
-
-    private void Update()
-    {
-        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
-        RaycastHit2D hit = Physics2D.Raycast(mousePos, Vector2.zero);
-    
-        if (hit.collider != null && hit.collider.CompareTag("Visitor"))
-        {
-            hoveredCharacter = hit.collider.gameObject;
-            // Optional: highlight character
-        }
-        else
-        {
-            hoveredCharacter = null;
-        }
-    }
-
-    private void OnEnable()
-    {
-        move.action.started += Move;
-        select.action.started += Select;
-    }
-
-    private void OnDisable()
-    {
-        move.action.started -= Move;
-        select.action.started -= Select;
-    }
-
-    private void Select(InputAction.CallbackContext obj)
-    {
-        if (hoveredCharacter != null)
-        {
-            Debug.Log("Selected: " + hoveredCharacter.name);
-            // Perform selection logic
-        }
-    }
-
-    private void Move(InputAction.CallbackContext obj)
-    {
-        Debug.Log("move");
-        gameObject.transform.position += new Vector3(100.0f, 0.0f, 0.0f);
-    }
 }
--- Assets/Scripts/Selectable.cs
+++ Assets/Scripts/Selectable.cs
@@ -1,30 +1,57 @@
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.SceneManagement;
 
 public class Selectable : MonoBehaviour
 {
+    [SerializeField]
+    private UnityEvent clicked;
+    private MouseInputProvider mouse;
+
+    private BoxCollider2D collider;
+    
     public GameObject visitorPanel;
     public Camera visitorCamera;
 
-    private Camera mainCam;
+    private Camera playerCamera;
     
     private void Awake()
     {
-        mainCam = Camera.main;
+        playerCamera = GameObject.Find("PlayerCamera").GetComponent<Camera>();
+        
+        collider = GetComponent<BoxCollider2D>();
+        Debug.Log("Box " + collider.bounds);
+        mouse = FindFirstObjectByType<MouseInputProvider>();
+        mouse.Clicked += MouseOnClicked;
+    }
+
+    private void MouseOnClicked()
+    {
+        Debug.Log("Mouse: " + mouse.WorldPosition + ", Collider: " + collider.bounds);
+        Debug.Log("MousePos: " + mouse.WorldPosition);
+        if (collider.OverlapPoint(mouse.WorldPosition))
+        {
+            Debug.Log("Invoke");
+            ZoomIn();
+        }
+        else
+        {
+            Debug.DrawLine(collider.bounds.min, collider.bounds.max, Color.red, 2f);
+            Debug.Log("click not in bounds");
+        }
     }
 
     public void ZoomIn()
     {
-        mainCam.enabled = false;
-        visitorCamera.enabled = true;
-        visitorPanel.SetActive(true);
-        SceneManager.LoadScene(SceneRefs.Instance.MainMenu);
+        playerCamera.enabled = false;
+        visitorCamera.enabled = true; 
+        //visitorPanel.SetActive(true);
     }
 
     public void ZoomOut()
     {
-        mainCam.enabled = true;
+        playerCamera.enabled = true;
         visitorCamera.enabled = false;
-        visitorPanel.SetActive(false);
+        //visitorPanel.SetActive(false);
     }
 }
