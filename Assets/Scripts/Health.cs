ec67d4a804761fe53a62044e1d1fb9dc18dedd86
Kristo Krikmann <kristo.krikmann@gmail.com>
2026-01-25 12:06:00 +0200

    push

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -3,17 +3,6 @@ using UnityEngine;
 
 public class Health : MonoBehaviour
 {
-    public static Health Instance;
     public int startingHP;
     public int currentHP;
-
-    private void Awake()
-    {
-        if (Instance == null)
-        {
-            Instance = this;
-            DontDestroyOnLoad(gameObject);
-        }
-        else Destroy(gameObject);
-    }
 }
