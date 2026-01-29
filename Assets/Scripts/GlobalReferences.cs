5a11c9c162446203ab027393a24f27a9b0694676
Kristo Krikmann <kristo.krikmann@gmail.com>
2026-01-29 15:10:39 +0200

    gameplay reworked

--- Assets/Scripts/GlobalReferences.cs
+++
@@ -1,5 +0,0 @@
-public static class GlobalReferences
-{
-    public static float volume = 0.5f;
-    public static AudioManager audioManager;
-}
--- Assets/Scripts/Health.cs
+++
@@ -1,8 +0,0 @@
-using System;
-using UnityEngine;
-
-public class Health : MonoBehaviour
-{
-    public int startingHP;
-    public int currentHP;
-}
